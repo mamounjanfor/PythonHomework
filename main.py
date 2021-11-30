f2a4389a965fee2394b15d7b375ae27e1edc2a74
mamounjanfor <mamounjanfor@gmail.com>
2021-11-30 15:55:00 -0500

    init

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Author : Mamoun Janfor
+create a function named even that will take 2 int parameters, one for starting point and the other is a counter
+return a list of counter smallest even int greater than or equal to starting point in ascending order
+"""
+
+
+def home_work(start, n):
+    if start % 2 == 0:
+        for i in range(0, n * 2, 2):
+            print(i + start)
+    else:
+        start = start + 1
+        for i in range(0, n * 2, 2):
+            print(i + start)
+
+
+if __name__ == '__main__':
+    a = int(input("enter a starting point: "))
+    b = int(input("enter a counter: "))
+    d = home_work(a, b)
+    print(d)
